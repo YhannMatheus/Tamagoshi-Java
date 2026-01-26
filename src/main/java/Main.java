be085b3a4cc844577363a0841afb8e05cf2246c6
caiiocb <caiob3870@gmail.com>
2026-01-26 19:23:30 -0300

    Métodos Especiais

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
+import Models.Pet;
+
 public class Main {
+    public static void main (String[] args){
+        Pet p = new Pet("Joy");
+        p.exibir();
+    }
 }
--- src/main/java/Models/Pet.java
+++ src/main/java/Models/Pet.java
@@ -5,11 +5,61 @@ public class Pet {
     private int life;
     private int hungry;
     private int cleaning;
+    private int fun;
+    private long timestamp;
+
+    public void exibir(){
+        System.out.println("Nome : " + this.getName());
+        System.out.println("Life: " + this.getLife());
+        System.out.println("Hungry: " + this.getHungry());
+        System.out.println("Fun: " + this.getFun());
+    }
 
     public Pet(String name) {
         this.name = name;
         this.life = 100;
-        this.hungry = 0;
-        this.cleaning = 0;
+        this.hungry = 80;
+        this.cleaning = 100;
+        this.fun = 50;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getHungry() {
+        return hungry;
+    }
+
+    public void setHungry(int hungry) {
+        this.hungry = hungry;
+    }
+
+    public int getCleaning() {
+        return cleaning;
+    }
+
+    public void setCleaning(int cleaning) {
+        this.cleaning = cleaning;
+    }
+
+    public int getFun() {
+        return fun;
+    }
+
+    public void setFun(int fun) {
+        this.fun = fun;
     }
 }
